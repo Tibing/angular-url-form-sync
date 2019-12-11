043cbcaaef95dce664c19436fff603ca292b410c
Tibing <nik.poltoratsky@gmail.com>
2019-12-11 09:18:34 +0300

    add impl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { pluck, tap } from 'rxjs/operators';
+import { merge, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'urlformmapping';
+  form = new FormControl('');
+
+  private queryFromForm$: Observable<string> = this.form.valueChanges
+    .pipe(
+      tap((query: string) => this.syncUrlState(query)),
+    );
+
+  private queryFromUrl$: Observable<string> = this.route.queryParams
+    .pipe(
+      pluck('query'),
+    );
+
+  resultingDataSet$: Observable<string> = merge(this.queryFromForm$, this.queryFromUrl$)
+    .pipe(
+      // query data or persist this query or do whatever
+    );
+
+  constructor(private route: ActivatedRoute,
+              private router: Router) {
+  }
+
+  private syncUrlState(query: string): void {
+    this.router.navigate(['/'], { queryParams: { query } });
+  }
 }
